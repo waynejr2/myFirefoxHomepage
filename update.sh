5990924c509fa605f7dcbaa947cbf1bce29dd270
wayne <wayne>
2017-04-14 18:27:44 -0700

    check for directory and tar

--- update.sh
+++ update.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-cp -v myHomepage.html E:\\myHomepage\\myHomepage.html
+DOWHOME=C:\\Users\\wayne\\Downloads\\myFirefoxHomepage-master-test
+if [ -d $DOWHOME ]
+then
+	echo $DOWHOME
+	tar cvf - * | (cd $DOWHOME ; tar xf - )
+fi
+
